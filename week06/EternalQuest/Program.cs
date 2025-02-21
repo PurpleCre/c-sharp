2a97579e88d5d287314d661bc5f3abc0d8fe2bac
PurpleCre <pgonese6@gmail.com>
2025-02-21 14:44:22 +0200

    EternalQuest: Complete

--- week06/EternalQuest/Program.cs
+++ week06/EternalQuest/Program.cs
@@ -1,9 +1,42 @@
 using System;
-
+// Added a parent GoalManager where all the different goal types are managed and all the inheritance logic and functionality are implemented
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the EternalQuest Project.");
+        Console.WriteLine("\nHello World! This is the EternalQuest Project.\n-------------------------------------------------------------------------------\n");
+        GoalManager manager = new GoalManager();
+        bool run = true;
+        bool quit;
+
+        do
+        {
+            manager.Start();
+
+            Console.Write("Are you sure you want to quit? (yes/no)\n>  ");
+            string rsp = Console.ReadLine();
+
+            do
+            {
+                quit = true;
+
+                if (rsp.ToLower() == "yes")
+                {
+                    run = false;
+                    Console.WriteLine("\nThanks for indulging the collective");
+                }
+                else if (rsp.ToLower() == "no")
+                {
+                    run = true;
+                }
+                else
+                {
+                    Console.Write("Invalid response, choose between (yes) and (no)\n>");
+                    rsp = Console.ReadLine();
+                    quit = false;
+                }
+            } while (!quit);
+
+        } while (run);
     }
 }
