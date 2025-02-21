102d9e0729bfe2e3594e8d5fcea0beadbb117ae9
PurpleCre <pgonese6@gmail.com>
2025-02-21 14:15:24 +0200

    Mindfulness: done

--- week05/Mindfulness/Program.cs
+++ week05/Mindfulness/Program.cs
@@ -1,9 +1,45 @@
 using System;
 
+// I added logic to make sure no questions or prompts are repeated in the same session
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Mindfulness Project.");
+        string banner = "Hello World! This is the Mindfulness Project.\n";
+        Console.WriteLine(banner);
+
+        bool play = true;
+        do
+        {
+             Console.WriteLine("Menu options:\n1. Start Breathing Actvity\n2. Start reflecting Activity\n3. Start Listing Activity\n4. Quit");
+             Console.Write("Choose an option from the menu: ");
+             string choice = Console.ReadLine();
+
+             int usrChoice = int.Parse(choice);
+
+             if(usrChoice == 1)
+             {
+                BreathingActivty breathing = new BreathingActivty();
+                breathing.Run();
+             }
+             else if(usrChoice == 2)
+             {
+                ReflectingActivty reflecting = new ReflectingActivty();
+                reflecting.Run();
+             }
+             else if(usrChoice == 3)
+             {
+                ListingActivity listing = new ListingActivity();
+                listing.Run();
+             }
+             else if(usrChoice == 4)
+             {
+                play = false;
+             }
+
+        } while (play);
+
+        Console.Clear();
+        Console.WriteLine("Bye Bye");
     }
 }
